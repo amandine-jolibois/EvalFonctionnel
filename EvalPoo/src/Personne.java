2488f8b8bc6161e924603fa6e9c7bdaf3d5a2d20
amandine-jolibois <joliboisamandine9@gmail.com>
2024-05-15 15:07:43 +0200

    Premier Commit du POO

---
+++ EvalPoo/src/Personne.java
@@ -0,0 +1,45 @@
+public abstract class Personne {
+
+	protected String nom;
+	protected String prenom;
+	protected int id;
+	
+	public Personne(String nom, String prenom, int id) {
+		super();
+		this.nom = nom;
+		this.prenom = prenom;
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public void afficherDetails() {
+		System.out.println("ID" + this.id);
+		System.out.println("Nom" + this.nom);
+		System.out.println("Prénom" + this.prenom);
+		
+	}
+	
+}
